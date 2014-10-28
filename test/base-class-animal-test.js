564f01685397b53e34f9dd400a4b7bda633278bc
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2014-10-29 00:47:01 +0900

    revise test script

--- test/base-class-animal-test.js
+++ test/base-class-animal-test.js
@@ -8,6 +8,9 @@
     var BaseClass = require('base-class-extend');
   }
 
+  var rex = /^[AIUEO]/i;
+  function a(s) { return rex.exec(s) ? 'an ' + s : 'a ' + s; }
+
   // SimpleClass
   var SimpleClass = BaseClass.extend();
   var s1 = new SimpleClass();
@@ -22,24 +25,25 @@
     get name() { return this._name; }, // getter
     set name(name) { this._name = name; }, // setter
     introduce: function () {
-      console.log('My name is ' + this.name);
+      console.log('My name is ' + this.name + '. ' +
+                  'I am ' + a(this.constructor.name) + '.');
     },
   }, {
     init: function () {
-      console.log('Animal class init');
+      console.log('Animal class init. (' + this.name + ')');
     },
     animalClassMethod: function () {
-      console.log('Animal class method');
+      console.log('Animal class method. (' + this.name + ')');
     }
-  }); // -> Animal class init
+  }); // -> Animal class init. (Animal)
   var a1 = new Animal('Annie');
-  a1.introduce(); // -> My name is Annie
-  Animal.animalClassMethod(); // -> Animal class method
+  a1.introduce(); // -> My name is Annie. I am an Animal.
+  Animal.animalClassMethod(); // -> Animal class method. (Animal)
 
   // Bear
   var Bear = Animal.extend('Bear');
   var b1 = Bear('Pooh'); // new less
-  b1.introduce(); // -> My name is Pooh
+  b1.introduce(); // -> My name is Pooh. I am a Bear.
 
   var Cat = Animal.extend({
     new: function Cat() {
@@ -49,7 +53,7 @@
     }
   });
   var c1 = Cat.new('Kitty');
-  c1.introduce(); // -> My name is Kitty
+  c1.introduce(); // -> My name is Kitty. I am a Cat.
 
   var Dog = Animal.extend({
     new: function Dog() {
@@ -59,14 +63,14 @@
     },
   }, {
     init: function () {
-      console.log('Dog class init');
+      console.log('Dog class init. (' + this.name + ')');
     },
     dogClassMethod: function () {
       this.animalClassMethod();
-      console.log('Dog class method');
+      console.log('Dog class method. (' + this.name + ')');
     }
-  }); // -> Dog init
+  }); // -> Dog class init. (Dog)
   var d1 = Dog.new('Hachi'); // Class method new call
-  d1.introduce(); // -> My name is Hachi
-  Dog.dogClassMethod(); // -> Animal class method, Dog class method
-  Dog.animalClassMethod(); // -> Animal class method
+  d1.introduce(); // -> My name is Hachi. I am a Dog.
+  Dog.dogClassMethod(); // -> Animal class method. (Animal), Dog class method. (Dog)
+  Dog.animalClassMethod(); // -> Animal class method. (Dog)
