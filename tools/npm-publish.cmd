0d0a2e9db82aa362f3905443b700dab77d0315d0
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2014-10-31 08:36:37 +0900

    modify npm publish command

--- tools/npm-publish.cmd
+++ tools/npm-publish.cmd
@@ -1,8 +1,9 @@
 @set HERE=%~dp0
 @pushd %HERE%
-call npm publish
+@cd ..
+@call npm publish
 node tools/string-replace base-class-extend new-base-class package.json README.md test/base-class-animal-test.js test/base-class-test-en.js test/base-class-test-jp.js test/base-class-vector-test.js
-call npm publish
+@call npm publish
 node tools/string-replace new-base-class base-class-extend package.json README.md test/base-class-animal-test.js test/base-class-test-en.js test/base-class-test-jp.js test/base-class-vector-test.js
 @popd
 pause
