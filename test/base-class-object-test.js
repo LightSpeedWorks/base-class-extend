c7c3aecbe2c9a8947dfa998c92661227a247d561
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2014-10-30 19:10:19 +0900

    modify test scripts

--- test/base-class-object-test.js
+++ test/base-class-object-test.js
@@ -8,25 +8,41 @@
     var BaseClass = require('base-class-extend');
   }
 
+  // BaseClass
   var b1 = new BaseClass();
   var b2 = BaseClass.new();
   var b3 = BaseClass();
 
-  var SimpleClass1 = BaseClass.extend.call(Object, 'SimpleClass1', {});
+  // Prepare
+  var extend = BaseClass.extend;
+
+  // SimpleClass0
+  var SimpleClass0 = extend('SimpleClass0');
+
+  var s01 = new SimpleClass0();
+  var s02 = SimpleClass0.new();
+  var s03 = SimpleClass0();
+
+  // SimpleClass1
+  var SimpleClass1 = BaseClass.extend.call(Object, 'SimpleClass1');
 
   var s11 = new SimpleClass1();
   var s12 = SimpleClass1.new();
   var s13 = SimpleClass1();
 
+  // Prepare for Object
   Object.extend = BaseClass.extend;
   Object.new = BaseClass.new;
-  var SimpleClass2 = Object.extend('SimpleClass2', {});
+
+  // SimpleClass2
+  var SimpleClass2 = Object.extend('SimpleClass2');
 
   var s21 = new SimpleClass2();
   var s22 = SimpleClass2.new();
   var s23 = SimpleClass2();
 
   checkConstructorName('BaseClass', b1, b2, b3);
+  checkConstructorName('SimpleClass0', s01, s02, s03);
   checkConstructorName('SimpleClass1', s11, s12, s13);
   checkConstructorName('SimpleClass2', s21, s22, s23);
 
@@ -34,9 +50,9 @@
     var objs = Array.prototype.slice.call(arguments, 1);
 
     objs.forEach(function (obj) {
-      console.log('%j %s', obj, obj?obj.constructor.name:'');
+      console.log(obj ? obj.constructor.name : '');
       if (obj == null) return;
       if (obj.constructor.name !== ctorName)
-        /*throw new TypeError*/ console.log(obj.constructor.name + ' !== ' + ctorName);
+        throw new TypeError(obj.constructor.name + ' !== ' + ctorName);
     });
   }
