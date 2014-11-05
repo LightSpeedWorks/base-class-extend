69582dcfd0cbbecb8b70b8f4c3f8eea0ff9c2dd4
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2014-11-06 00:13:01 +0900

    add test scripts

--- tools/npm-publish.cmd
+++ tools/npm-publish.cmd
@@ -2,8 +2,8 @@
 @pushd %HERE%
 @cd ..
 @call npm publish
-node tools/string-replace base-class-extend new-base-class package.json README.md test/quick.js test/animal-test.js test/test-en.js test/test-jp.js test/vector-test.js test/function-test.js test/object-test.js test/private-test.js
+node tools/string-replace base-class-extend new-base-class package.json README.md test/quick.js test/animal-test.js test/test-en.js test/test-jp.js test/vector-test.js test/function-test.js test/object-test.js test/private-test.js test/vector-private-test.js
 @call npm publish
-node tools/string-replace new-base-class base-class-extend package.json README.md test/quick.js test/animal-test.js test/test-en.js test/test-jp.js test/vector-test.js test/function-test.js test/object-test.js test/private-test.js
+node tools/string-replace new-base-class base-class-extend package.json README.md test/quick.js test/animal-test.js test/test-en.js test/test-jp.js test/vector-test.js test/function-test.js test/object-test.js test/private-test.js test/vector-private-test.js
 @popd
 pause
