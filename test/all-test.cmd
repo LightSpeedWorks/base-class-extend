de022122c66f705fd59f1720685bb3a7c2814c34
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2015-01-14 19:24:21 +0900

    add all test script

---
+++ test/all-test.cmd
@@ -0,0 +1,12 @@
+start web-browser-public-test.html
+start web-browser-test.html
+node test-en.js
+node test-jp.js
+node quick.js
+node animal-test.js
+node function-test.js
+node object-test.js
+node private-test.js
+node vector-private-test.js
+node vector-test.js
+pause
