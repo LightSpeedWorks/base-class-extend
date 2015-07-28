e8b8b7398e4691d432f6c32ea5e4cd065dab219c
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2015-07-29 08:05:00 +0900

    node util.inherits

---
+++ work/inherits.js
@@ -0,0 +1,13 @@
+// v0.10.38
+
+function (ctor, superCtor) {
+  ctor.super_ = superCtor;
+  ctor.prototype = Object.create(superCtor.prototype, {
+    constructor: {
+      value: ctor,
+      enumerable: false,
+      writable: true,
+      configurable: true
+    }
+  });
+}
---
+++ work/inherits2.js
@@ -0,0 +1,26 @@
+// v2.2.1
+
+function (ctor, superCtor) {
+
+  if (ctor === undefined || ctor === null)
+    throw new TypeError('The constructor to `inherits` must not be ' +
+                        'null or undefined.');
+
+  if (superCtor === undefined || superCtor === null)
+    throw new TypeError('The super constructor to `inherits` must not ' +
+                        'be null or undefined.');
+
+  if (superCtor.prototype === undefined)
+    throw new TypeError('The super constructor to `inherits` must ' +
+                        'have a prototype.');
+
+  ctor.super_ = superCtor;
+  ctor.prototype = Object.create(superCtor.prototype, {
+    constructor: {
+      value: ctor,
+      enumerable: false,
+      writable: true,
+      configurable: true
+    }
+  });
+}
